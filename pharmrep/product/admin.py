5c749eef3239dd1357b39276a7bc369533d618e0
bayo <bayokrapht@gmail.com>
2016-04-21 08:49:39 +0100

    added product models to admin app

--- pharmrep/product/admin.py
+++ pharmrep/product/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from product.models import Product, Rep, Customer, Sale, Payment
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rate']
+
+
+@admin.register(Rep, Customer)
+class PeopleAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ['product', 'rep', 'customer', 'quantity',
+                    'sales_date', 'recorded_date']
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['amount', 'rep', 'customer', 'receipt_no',
+                    'payment_date', 'recorded_date', 'balance']
