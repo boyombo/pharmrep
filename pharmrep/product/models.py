21b7488a401de5948826541678a3233a81e5c480
bayo <bayokrapht@gmail.com>
2016-04-21 08:43:06 +0100

    made basic models

---
+++ pharmrep/product/models.py
@@ -0,0 +1,53 @@
+from __future__ import unicode_literals
+from datetime import datetime
+
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    rate = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
+
+
+class Rep(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.TextField(blank=True)
+    phone1 = models.CharField(max_length=20, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Sale(models.Model):
+    rep = models.ForeignKey(Rep, related_name='rep_sales')
+    customer = models.ForeignKey(Customer, related_name='customer_sales')
+    product = models.ForeignKey(Product, related_name='product_sales')
+    quantity = models.PositiveIntegerField()
+    sales_date = models.DateField()
+    recorded_date = models.DateTimeField(default=datetime.now)
+
+    def __unicode__(self):
+        return unicode(self.customer)
+
+
+class Payment(models.Model):
+    rep = models.ForeignKey(Rep, related_name='rep_payments')
+    customer = models.ForeignKey(Customer, related_name='customer_payments')
+    amount = models.PositiveIntegerField()
+    receipt_no = models.CharField(max_length=50, blank=True)
+    payment_date = models.DateField()
+    recorded_date = models.DateTimeField(default=datetime.now)
+    balance = models.IntegerField()
+
+    def __unicode__(self):
+        return unicode(self.customer)
