fce45ba14c750d8316214eeb59f535a2c127b6c2
bayo <bayokrapht@gmail.com>
2016-04-21 22:52:52 +0100

    changed the db to postgres

--- pharmrep/pharmrep/settings.py
+++ pharmrep/pharmrep/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'pharmrep.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bayo_pharmrep',
+        'USER': 'bayo_pharmrep',
+        'PASSWORD': 'pass.p455',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
