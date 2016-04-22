20f279fae85684cb168539b28f23decd0e9f6116
bayo <bayokrapht@gmail.com>
2016-04-22 08:21:11 +0100

    sales form processing

--- pharmrep/product/views.py
+++ pharmrep/product/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from product.forms import SaleForm, PaymentForm
+from product.models import Rep
 
 
 @login_required
 def sale(request):
-    form = SaleForm()
+    rep = get_object_or_404(Rep, user=request.user)
+    if request.method == 'POST':
+        form = SaleForm(request.POST)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.rep = rep
+            obj.save()
+    else:
+        form = SaleForm()
     return render(request, 'product/sale.html', {'form': form})
 
 
