637c583619ff664f71aae1ac25bcc433ec3f19a9
bayo <bayokrapht@gmail.com>
2016-04-29 11:52:06 +0100

    added charts to report

--- pharmrep/reports/urls.py
+++ pharmrep/reports/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from reports import views
 
@@ -7,4 +8,10 @@ urlpatterns = [
     url(r'balance/$', views.balance, name='report_balance'),
     url(r'performance/$', views.performance, name='report_performance'),
     url(r'collection/$', views.collection, name='report_collection'),
+    url(r'saleschart/$', TemplateView.as_view(
+        template_name='reports/sales_chart.html'), name='chart_sales'),
+    url(r'paymentchart/$', TemplateView.as_view(
+        template_name='reports/payment_chart.html'), name='chart_payment'),
+    url(r'callchart/$', TemplateView.as_view(
+        template_name='reports/calls_chart.html'), name='chart_call'),
 ]
