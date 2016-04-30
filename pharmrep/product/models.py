cfaccafe3d262548b0e20359d1dce58b369117f3
bayo <bayokrapht@gmail.com>
2016-04-30 04:31:16 +0100

    migrations to change sales and payment models, datepicker autoclose

--- pharmrep/product/models.py
+++ pharmrep/product/models.py
@@ -33,9 +33,23 @@ class Rep(models.Model):
 
 
 class Customer(models.Model):
+    HOSPITAL = 0
+    INSTITUTION = 1
+    PHARMACY = 2
+    WHOLESELLER = 3
+    HEALTH_PERSONNEL = 4
+    CUSTOMER_TYPE = (
+        (0, 'Hospital'),
+        (1, 'Institution'),
+        (2, 'Pharmacy'),
+        (3, 'Wholeseller'),
+        (4, 'Health Personnel'))
     name = models.CharField(max_length=200)
     address = models.TextField(blank=True)
+    contact_person = models.CharField(max_length=200, blank=True)
     phone1 = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(blank=True, null=True)
+    customer_type = models.PositiveIntegerField(choices=CUSTOMER_TYPE)
 
     def __unicode__(self):
         return self.name
@@ -50,12 +64,19 @@ class Customer(models.Model):
 
 
 class Sale(models.Model):
+    ACTUAL_SALES = 0
+    SOR = 1
+    SAMPLES = 2
+    SALES_TYPES = ((0, 'Actual Sales'), (1, 'SOR'), (2, 'Samples'))
     rep = models.ForeignKey(Rep, related_name='rep_sales')
     customer = models.ForeignKey(Customer, related_name='customer_sales')
     product = models.ForeignKey(Product, related_name='product_sales')
     quantity = models.PositiveIntegerField()
     amount = models.IntegerField()
+    invoice_no = models.CharField(max_length=200, blank=True)
+    invoice_date = models.DateField(blank=True, null=True)
     sales_date = models.DateField()
+    sales_type = models.PositiveIntegerField(choices=SALES_TYPES)
     recorded_date = models.DateTimeField(default=datetime.now)
 
     def __unicode__(self):
@@ -63,13 +84,22 @@ class Sale(models.Model):
 
 
 class Payment(models.Model):
+    EPAYMENT = 0
+    CHEQUE = 1
+    TELLER = 2
+    MODE_OF_PAYMENT = ((0, 'E-Payment'), (1, 'Cheque'), (2, 'Teller'))
+
     rep = models.ForeignKey(Rep, related_name='rep_payments')
     customer = models.ForeignKey(Customer, related_name='customer_payments')
     amount = models.PositiveIntegerField()
     receipt_no = models.CharField(max_length=50, blank=True)
     payment_date = models.DateField()
+    receipt_date = models.DateField()
     recorded_date = models.DateTimeField(default=datetime.now)
     balance = models.IntegerField()
+    mode_of_payment = models.PositiveIntegerField(choices=MODE_OF_PAYMENT)
+    teller_number = models.CharField(max_length=50, blank=True)
+    teller_date = models.DateField(blank=True, null=True)
     remarks = models.TextField(blank=True)
 
     def __unicode__(self):
